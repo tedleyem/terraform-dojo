a46a30ed8377e83fdab98aa580266a8b1cc93fd5
Tedley <tmeralus@gmail.com>
2023-11-05 14:59:55 -0500

    changed name of tf files

--- minikube-poc-deployment/main.tf
+++ minikube-poc-deployment/main.tf
@@ -1,14 +1,52 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.11.0"
-    }
+resource "kubernetes_namespace" "tf-nginx-ns" {
+  metadata {
+    name = "tf-namespace"
   }
 }
 
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "minikube"
+resource "kubernetes_deployment" "test-deploy" {
+  metadata {
+    name = "terraform-test-deployment"
+    labels = {
+      test = "TestApp"
+    }
+    namespace = "tf-namespace"
+  }
+
+  spec {
+    replicas = 2
+
+    selector {
+      match_labels = {
+        test = "TestApp"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          test = "TestApp"
+        }
+      }
+
+      spec {
+        container {
+          image = "nginx:1.21.6"
+          name  = "nginx"
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
